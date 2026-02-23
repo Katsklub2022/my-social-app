44ec70d344c654a4bef9b03e7cff56af5eef40f6
CheEska <99469288+Katsklub2022@users.noreply.github.com>
2026-02-23 16:50:05 +0800

    Update server to use platform-provided PORT

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+// IMPORTANT: gamitin yung port na ibibigay ng platform
+const PORT = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
